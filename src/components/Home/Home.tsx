47f6fdfceddcc15744093bd93d7bd8f07abc9f2d
JonathanDPotter <jonathan.d.potter@outlook.com>
2022-05-04 15:18:17 -0400

    displays current users

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,8 @@
 import React, { FormEvent, useEffect, useState } from "react";
-import api from "../../api";
+import { v4 as uuid } from "uuid";
+import { useBeforeunload } from "react-beforeunload";
 // utils
+import api from "../../api";
 import { useSockets } from "../../context/socket.context";
 import { useUser } from "../../context/user.context";
 // styles
@@ -9,25 +11,31 @@ import "./Home.scss";
 const Home = () => {
   const { user, token, setUser, setToken } = useUser();
   // local state
+  const [users, setUsers] = useState<string[] | null>(null);
   const [message, setMessage] = useState("");
   const [response, setResponse] = useState([
-    { name: "name", message: "message" },
+    { name: "name", message: "message", id: "id" },
   ]);
 
   const { socket } = useSockets();
 
-  socket.on("connection", (message) => {
-    setResponse([...response, message]);
+  socket.on("connection", ({ name, message, id }) => {
+    setResponse([...response, { name, message, id }]);
+    setUsers(users);
   });
 
   socket.on("message", (received) => {
     setResponse([...response, received]);
   });
 
+  socket.on("new", ({ users }) => {
+    setUsers(users);
+  });
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     socket.emit("message", { name: user, message });
-    setResponse([...response, { name: "Me", message }]);
+    setResponse([...response, { name: "Me", message, id: uuid() }]);
     setMessage("");
   };
 
@@ -39,20 +47,40 @@ const Home = () => {
   };
 
   useEffect(() => {
-    user && socket.emit("new", { name: user, message: "" });
-  }, [user]);
+    // users kept connecting multiple times until I added the setTimeout
+    if (!socket.connected) window.setTimeout(() => socket.connect(), 500);
+    socket.emit("new", { name: user });
 
-  useEffect(() => {
-    const validateUser = (async () => {
+    (async () => {
       if (token) {
         const valid = await api.validate(token);
         if (!valid.data.success) removeUser();
       }
     })();
+    window.addEventListener("beforeunload", (event) => {
+      event?.preventDefault();
+      return "";
+    });
+
+    window.addEventListener("unload", () => socket.disconnect());
+
+    return () => {
+      setUsers(null);
+      socket.disconnect();
+      window.removeEventListener("beforeunload", (event) => {
+        event?.preventDefault();
+        return "";
+      });
+      window.removeEventListener("unload", () => socket.disconnect());
+    };
   }, []);
 
   return (
     <div>
+      <div className="current-users">
+        <h2>Current Users</h2>
+        {users && users.map((usr) => <p key={usr}>{usr}</p>)}
+      </div>
       <form action="submit" onSubmit={handleSubmit}>
         <div className="label-input">
           <label htmlFor="message">Enter a message </label>
@@ -71,11 +99,7 @@ const Home = () => {
         {response.map(
           (res, i) =>
             i > 0 && (
-              <p
-                key={`response ${i}`}
-                id={res.name.toLowerCase()}
-                className="message"
-              >
+              <p key={res.id} id={res.name.toLowerCase()} className="message">
                 {res.name}:{res.message}
               </p>
             )
